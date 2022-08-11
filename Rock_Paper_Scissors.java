30bb916c5d2671843e084ecbe161a188c231d7f5
gary-programing <garybrolly@gmail.com>
2022-08-11 14:07:00 +0100

    02: completed program

--- Rock_Paper_Scissors.java
+++ Rock_Paper_Scissors.java
@@ -6,13 +6,37 @@ public class Rock_Paper_Scissors {
     public static void main(String[] args) {
             String playerChoice = RPS_player();
             String compChoice = RPS_comp();
+            String winner = Winner(playerChoice, compChoice);
+            System.out.println(winner);
+
+    }
+
+    private static String Winner(String playerChoice, String compChoice) {
+
+        String winner = "";
+        if (playerChoice == compChoice){
+            winner = "we have a tie";
+        }else if (playerChoice.equals("rock")  && compChoice.equals("paper")){
+            winner = "the winner was comp who played: "+compChoice;
+        }else if (playerChoice.equals("paper")  && compChoice.equals("rock")){
+            winner = "the winner was Player who played: "+playerChoice;
+        }else if (playerChoice.equals("rock")  && compChoice.equals("Scissors")) {
+            winner = "the winner was player"+playerChoice;
+        }else if (playerChoice.equals("Scissors")  && compChoice.equals("rock")) {
+            winner = "the winner was comp who played: "+compChoice;
+        }else if (playerChoice.equals("Scissors")  && compChoice.equals("paper")) {
+            winner = "the winner was player who played: "+playerChoice;
+        }else{
+            winner = "the winner was comp who played: "+compChoice;
+        }
+
+        return winner;
     }
 
     private static String RPS_comp() {
         String compChoice ="";
         Random randomizer = new Random();
-        int num = randomizer.nextInt(3)+1;;
-        System.out.println(num);
+        int num = randomizer.nextInt(3)+1;
         switch (num){
             case 1:
                 compChoice = "rock";
@@ -26,11 +50,10 @@ public class Rock_Paper_Scissors {
             default:
                 System.out.println("please enter a valid input");
         }
-        System.out.println(compChoice);
         return compChoice;
     }
 
-    public static String RPS_player(){
+    private static String RPS_player(){
         System.out.println("Please choose rock, paper or scissors by entering the number:\n 1. rock \n 2. paper \n 3. Scissors");
         Scanner Choice = new Scanner(System.in);
         String num = Choice.nextLine();
@@ -48,9 +71,9 @@ public class Rock_Paper_Scissors {
             default:
                 System.out.println("please enter a valid input");
         }
-        System.out.println(playerChoice);
         return (playerChoice);
     }
+
 
 
 }
