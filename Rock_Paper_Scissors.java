7ad2435616257520a013b78dfd67cb98ad0971c0
gary-programing <garybrolly@gmail.com>
2022-08-11 15:37:14 +0100

    03: actually completed program

--- Rock_Paper_Scissors.java
+++ Rock_Paper_Scissors.java
@@ -4,10 +4,46 @@ import java.util.Scanner;
 public class Rock_Paper_Scissors {
 
     public static void main(String[] args) {
+        Scanner rounds = new Scanner(System.in);
+        System.out.println("Please enter how many rounds you'd like 1-10: ");
+        int i = 0;
+        String Rounds = rounds.nextLine();
+        try {
+             i = Integer.parseInt(Rounds);
+        }
+        catch (Exception e){
+            System.out.println("please enter int");
+        }
+        if (i <1 || i>10){
+            System.out.println("please make sure value is within range");
+            i=0;
+        }
+        int k = 0;
+        while (k < i) {
             String playerChoice = RPS_player();
             String compChoice = RPS_comp();
             String winner = Winner(playerChoice, compChoice);
-            System.out.println(winner);
+            int tie_num = 0;
+            int comp_wins = 0;
+            int player_wins = 0;
+            if (winner.equals("tie")) {
+                System.out.println("it was a tie");
+                tie_num = tie_num + 1;
+            } else if (winner.equals("comp")) {
+                System.out.println("the computer won this round");
+                comp_wins = comp_wins + 1;
+            } else {
+                System.out.println("player wins this round");
+                player_wins = player_wins + 1;
+            }
+            System.out.println(k);
+            if (player_wins > comp_wins){
+                System.out.println("the player was the overall winner with " +player_wins+" wins");
+            }else{
+                System.out.println("the computer was the overall winner with " +comp_wins+" wins");
+            }
+            k++;
+        }
 
     }
 
@@ -17,17 +53,17 @@ public class Rock_Paper_Scissors {
         if (playerChoice == compChoice){
             winner = "we have a tie";
         }else if (playerChoice.equals("rock")  && compChoice.equals("paper")){
-            winner = "the winner was comp who played: "+compChoice;
+            winner = "the winner was comp";
         }else if (playerChoice.equals("paper")  && compChoice.equals("rock")){
-            winner = "the winner was Player who played: "+playerChoice;
+            winner = "Player";
         }else if (playerChoice.equals("rock")  && compChoice.equals("Scissors")) {
-            winner = "the winner was player"+playerChoice;
+            winner = "player";
         }else if (playerChoice.equals("Scissors")  && compChoice.equals("rock")) {
-            winner = "the winner was comp who played: "+compChoice;
+            winner = "comp";
         }else if (playerChoice.equals("Scissors")  && compChoice.equals("paper")) {
-            winner = "the winner was player who played: "+playerChoice;
+            winner = "player";
         }else{
-            winner = "the winner was comp who played: "+compChoice;
+            winner = "comp";
         }
 
         return winner;
@@ -36,7 +72,8 @@ public class Rock_Paper_Scissors {
     private static String RPS_comp() {
         String compChoice ="";
         Random randomizer = new Random();
-        int num = randomizer.nextInt(3)+1;
+        int num = randomizer.nextInt(3)+1;;
+        System.out.println(num);
         switch (num){
             case 1:
                 compChoice = "rock";
@@ -50,6 +87,7 @@ public class Rock_Paper_Scissors {
             default:
                 System.out.println("please enter a valid input");
         }
+        System.out.println(compChoice);
         return compChoice;
     }
 
@@ -71,6 +109,7 @@ public class Rock_Paper_Scissors {
             default:
                 System.out.println("please enter a valid input");
         }
+        System.out.println(playerChoice);
         return (playerChoice);
     }
 
